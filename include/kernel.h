3c787d30bac7ddd9057877836f4b4a74bea92a70
Andy Ross <andrew.j.ross@intel.com>
2020-04-03 15:39:25 -0700

    kernel: Add k_mem_pool compatibility layer on top of k_heap
    Add a shim layer implementing the legacy k_mem_pool APIs backed by a
    k_heap instead of the original implementation.
    
    Signed-off-by: Andy Ross <andrew.j.ross@intel.com>

--- include/kernel.h
+++ include/kernel.h
@@ -4790,6 +4790,11 @@ void k_heap_free(struct k_heap *h, void *mem);
  * If the pool is to be accessed outside the module where it is defined, it
  * can be declared via
  *
+ * @note Note: when CONFIG_MEM_POOL_HEAP_BACKEND is enabled, the
+ * k_mem_pool API is implemented on top of a k_heap, which is a more
+ * general purpose allocator which does not make the same promises
+ * about splitting or alignment detailed above.
+ *
  * @code extern struct k_mem_pool <name>; @endcode
  *
  * @param name Name of the memory pool.
